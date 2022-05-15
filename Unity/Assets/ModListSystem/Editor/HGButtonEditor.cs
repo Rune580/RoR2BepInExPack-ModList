d72d3f4f85026c25a22cc27ac5cf1d3a6269b87b
Rune580 <12521934+Rune580@users.noreply.github.com>
2022-05-15 13:18:14 -0400

    Setup unity project

---
+++ Unity/Assets/ModListSystem/Editor/HGButtonEditor.cs
@@ -0,0 +1,13 @@
+﻿using RoR2.UI;
+
+namespace ModListSystem.Editor
+{
+    [UnityEditor.CustomEditor(typeof(HGButton))]
+    public class HGButtonEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    } 
+}
